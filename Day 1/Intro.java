29f6ce92cec261aace5bbd1ecfaa9ed5ddddee34
Huseyin DAYANIR <hsyndayanir@gmail.com>
2021-05-22 09:54:50 +0300

    Summary

--- Day 1/Intro.java
+++ Day 1/Intro.java
@@ -7,7 +7,7 @@ public class ornek {
 	public static void main(String[] args) {  
 		int i=1;
 		while(i<10) {  
-			System.out.println(i);
+			System.out.println(i); 
 			i++;
 		}
 		System.out.println("While döngüsü bitti");
